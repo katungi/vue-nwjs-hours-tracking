d0cc4d68691ec1d6194995ef0ae0861e14cfea7f
katungi <dankatdennis@gmail.com>
2020-07-24 18:14:46 +0300

    added a table to hours log

--- src/views/HoursLog.vue
+++ src/views/HoursLog.vue
@@ -1,7 +1,45 @@
 <template>
-  <b-container class= "hours-log-page">
-      <b-row>
-          
-      </b-row>
+  <b-container class="hours-log-page">
+    <b-row>
+      <b-col sm="8" offsetSm="2">
+        <b-row class="mt-5 mb-2">
+          <b-col>
+            <h1>Hours Log</h1>
+          </b-col>
+          <b-col>
+            <b-btn variant="primary">+ Add Entry</b-btn>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-table striped hover :items="items" />
+          </b-col>
+        </b-row> 
+      </b-col>
+    </b-row>
   </b-container>
 </template>
+<<<<<<< HEAD
+=======
+
+<script>
+export default {
+  name: 'HoursLog',
+  components: {
+  },
+  // eslint-disable-next-line no-restricted-syntax
+  data () {
+    return {
+      items: [
+        {
+          project_name: 'Company #1',
+          date: 'May 12, 2020', 
+          start_time: '7.00 am', 
+          end_time: '11.00 am'
+        }
+      ]
+    };
+  }
+};
+</script>
+>>>>>>> e3a5f9c... added a table to hours log
