08cdf8270de903fd88b3248309644e612f70f0c7
katungi <dankatdennis@gmail.com>
2020-07-25 08:37:40 +0300

    switched to different time picker

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import vSelect from 'vue-select';
+import DatePicker from 'vue2-datepicker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'vue-select/dist/vue-select.css';
+import 'vue2-datepicker/index.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -12,6 +14,7 @@ import store from './store';
 Vue.use(BootstrapVue);
 
 Vue.component('v-select', vSelect);
+Vue.component('date-picker', DatePicker);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
--- src/views/TimeEntry.vue
+++ src/views/TimeEntry.vue
@@ -10,11 +10,11 @@
         <b-row>
           <b-col>
             <b-form @submit="onSubmit" @reset="onReset">
-              <b-form-group id="input-group-2" label="Start Date:" labelFor="input-2">
-                <b-form-timepicker v-model="formData.start" id=" input-2" />
+              <b-form-group id="input-group-1" label="Start Date:">
+                <date-picker v-model="formData.startDate" type="datetime" />
               </b-form-group>
-              <b-form-group id="input-group-2" label="End Date:" labelFor="input-2">
-                <b-form-timepicker v-model="formData.end" id=" input-3" />
+              <b-form-group id="input-group-2" label="End Date:">
+                <date-picker v-model="formData.endDate" type="datetime" />
               </b-form-group>
               <b-button type="submit" variant="primary">Submit</b-button>
               <b-button type="reset" variant="danger">Reset</b-button>
@@ -31,6 +31,7 @@ export default {
   data () {
     return {
       formData: {
+        startDate
       }
     };
   },
